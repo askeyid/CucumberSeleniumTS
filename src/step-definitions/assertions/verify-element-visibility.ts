6bcde84057b2b67b109949257dcfc389de7eecf0
Aleksandr Malygon <a_m_style@outlook.com>
2024-03-27 17:21:15 +0200

    add custom waitFor. remove Chai

--- src/step-definitions/assertions/verify-element-visibility.ts
+++ src/step-definitions/assertions/verify-element-visibility.ts
@@ -1,28 +1,9 @@
 import { Then } from '@cucumber/cucumber'
-import { expect } from 'chai'
-import { By } from 'selenium-webdriver'
 import { ScenarioWorld } from '../setup/world';
-import { ElementKey, ExpectedElementText } from '../../env/global';
+import { ElementKey } from '../../env/global';
 import { getElementLocator } from '../../support/web-element-helper';
-
-Then(
-    /^the "(.*)" should contain the text "([^"]*)"$/,
-    async function(this: ScenarioWorld, elementKey: ElementKey, expectedElementText: ExpectedElementText) {
-        const {
-            screen: { driver },
-            globalVariables,
-            globalConfig
-        } = this;
-
-        console.log(`the ${elementKey} should contain the text ${expectedElementText}`);
-
-        const elementIdentifier = await getElementLocator(driver, elementKey, globalVariables, globalConfig);
-        const element = await driver.findElement(By.css(elementIdentifier));        
-        const elementText = await element.getAttribute('innerText');
-
-        expect(elementText).to.contain(expectedElementText);
-    }
-);
+import { waitFor } from '../../support/wait-for-behaviour';
+import { elementDisplayed } from '../../support/html-behaviour';
 
 Then(
     /^the "([^"]*)" should be displayed$/,
@@ -37,7 +18,9 @@ Then(
 
         const elementIdentifier = await getElementLocator(driver, elementKey, globalVariables, globalConfig);
 
-        const element = await driver.findElement(By.css(elementIdentifier));
-        expect(await element.isDisplayed()).to.be.true;
+        await waitFor(async () => {
+            const isElementVisible = await elementDisplayed(driver, elementIdentifier);
+            return isElementVisible;
+        });
     }
 );
