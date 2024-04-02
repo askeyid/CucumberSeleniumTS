e43bb22ad43e494a11a6274b6cb3b58eb52d4b8a
Aleksandr Malygon <a_m_style@outlook.com>
2024-04-02 17:00:37 +0300

    add stored values support

--- src/step-definitions/setup/world.ts
+++ src/step-definitions/setup/world.ts
@@ -3,7 +3,7 @@ import { World, IWorldOptions, setWorldConstructor } from '@cucumber/cucumber'
 import firefox from 'selenium-webdriver/firefox'
 import { Options } from 'selenium-webdriver/chrome'
 import { env } from '../../env/parseEnv'
-import { GlobalConfig } from '../../env/global'
+import { GlobalConfig, GlobalVariables } from '../../env/global'
 import { stringIsOfOptions } from '../../support/option-helper'
 
 export type Screen = {
@@ -15,9 +15,13 @@ export class ScenarioWorld extends World {
         super(options);
 
         this.globalConfig = options.parameters as GlobalConfig;
+
+        this.globalVariables = {}
     }
 
     globalConfig: GlobalConfig;
+
+    globalVariables: GlobalVariables;
     
     screen!: Screen;
 
---
+++ src/step-definitions/stored.ts
@@ -0,0 +1,35 @@
+import { waitFor, waitForSelector } from "../support/wait-for-behaviour";
+import { Then } from "@cucumber/cucumber";
+import { getElementLocator } from "../support/web-element-helper";
+import { ScenarioWorld } from "./setup/world";
+import { ElementKey, GlobalVariableKey } from "../env/global";
+import { getElementText } from "../support/html-behaviour";
+
+Then(
+    /^I retrieve the "([^"]*)" text and store it as "([^"]*)" in global variables$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey, globalVariableKey: GlobalVariableKey){
+        const {
+            screen: { driver },
+            globalConfig,
+            globalVariables
+        } = this;
+
+        console.log(`I retrive the ${elementKey} text and store it as ${globalVariableKey} in global variables`);
+
+        const elementIdentifier = await getElementLocator(driver, elementKey, globalConfig);
+
+        await waitFor(async () => {
+
+            const elementStable = await waitForSelector(driver, elementIdentifier);
+
+            if (elementStable) {
+                const elementText = await getElementText(driver, elementIdentifier);
+                if (elementText != null) {
+                    globalVariables[globalVariableKey] = elementText;
+                }
+            }
+
+            return elementStable;
+        })
+    }
+)
